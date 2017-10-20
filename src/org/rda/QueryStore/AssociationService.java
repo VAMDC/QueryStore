17b13848961611aa7c6179d1a83d5209f6c8ad0e
Carlo Maria Zwölf <carlo-maria.zwolf@obspm.fr>
2017-10-20 15:09:29 +0200

    Update AssociationService.java

--- src/org/rda/QueryStore/AssociationService.java
+++ src/org/rda/QueryStore/AssociationService.java
@@ -74,7 +74,7 @@ public class AssociationService extends HttpServlet {
 		response.setCharacterEncoding("UTF-8");
 
 		String userEmail = request.getParameter(EMAIL);
-		String userIP = request.getRemoteAddr();
+		String userIP = getUserIP(request);
 		String usedClient = request.getParameter(USED_CLIENT);
 		String queryToken = request.getParameter(QUERY_TOKEN);
 		
@@ -154,6 +154,26 @@ public class AssociationService extends HttpServlet {
 			toReturn = true;
 		}
 		return toReturn;
+	}
+	
+	private String getUserIP(HttpServletRequest request) {
+		String ip = request.getHeader("X-Forwarded-For");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
 	}
 
 }
